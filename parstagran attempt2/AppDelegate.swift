9a3efc0229ce921de6a942c5446afee79b7365f0
joed <joed.ngangmeni@bison.howard.edu>
2021-03-15 17:02:25 -0400

    Finished login and signup
    Starting camera view page

--- parstagran attempt2/AppDelegate.swift
+++ parstagran attempt2/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "KeZ9RnMRcHxGlSl0JJ2QCyCc8YFCdu9VZUlSLDtN" // <- UPDATE
+                    $0.clientKey = "loGwBf8qq4aQhPs3gUnWJJ6OYh7zzlwTtr27Lu7t" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
         return true
     }
 
